40a3a513f3f461f2557d1fbc642bcb4edfffc58d
Mirwangjn <138436253+Mirwangjn@users.noreply.github.com>
2024-10-19 13:37:38 +0800

    10. 解决gittalk问题

--- docs/.vitepress/gitalk.ts
+++ docs/.vitepress/gitalk.ts
@@ -1,6 +1,13 @@
 import Gitalk from 'gitalk';
 import 'gitalk/dist/gitalk.css';
 
+const generateId = (path) => {
+    return path
+        .split('/') // 按照 / 切分
+        .pop() // 取最后一个部分
+        .replace(/\.html$/, ''); // 去掉结尾的 .html
+};
+
 export default function createGitalk(path: string) {
     const gitalk = new Gitalk({
         clientID: 'f3c2300c17cf97cc6a4e',
@@ -8,8 +15,10 @@ export default function createGitalk(path: string) {
         repo: 'Note',
         owner: 'Mirwangjn',
         admin: ['Mirwangjn'],
-        id: path,      // 确保唯一性和长度小于 50
+        id: generateId(path),      // 确保唯一性和长度小于 50
         distractionFreeMode: false,  // 类似 facebook 的无干扰模式
     });
+    console.log(generateId(path));
+
     gitalk.render('gitalk-container');
 }
